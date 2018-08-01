655529050a742d05578c25585cd43d9fffc982e5
Louie Arteta <rfarteta@gmail.com>
2018-08-02 06:15:06 +0800

    Add Google Auth to Devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,16 +10,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    google_response = request.env['omniauth.auth']
-    @person = Person.new(build_name(google_response))
-    @user = User.from_omniauth(google_response, @person)
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      session['devise.google_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
-  belongs_to :person, class_name: 'Person'
+  belongs_to :person
 
   def self.from_omniauth(auth, person)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
