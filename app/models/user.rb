55f31a4ac8d240cc9044214ea0f1e6b7d5b212ed
cfcpoblete <cfcpoblete@gmail.com>
2018-08-04 21:22:09 +0800

    Google authentication fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
   belongs_to :person, class_name: 'Person'
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, person)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      return unless person.save
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.image = auth.info.image
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.person_id = person.id
       user.save!
     end
   end
